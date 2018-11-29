2d0a50e947ce8440c759c384cf53b9e4360d593f
Knut Zuidema <knut.zuidema@gmail.com>
2018-11-29 14:11:11 +0100

    fix method usage

--- src/game_map.py
+++ src/game_map.py
@@ -43,7 +43,7 @@ class GameMap:
         health_bar_window = curses.newwin(3, 23, map_size[0] + 2, 3)
         health_bar_window.border()
         health_bar_window.addstr(1, 2, "HP: ")
-        health_bar_window.addstr(1, 6, self.build_health_bar(40))
+        health_bar_window.addstr(1, 6, self.health_bar)
         health_bar_window.addstr(1, 17, "100")
         # self.player.power
         game_map_win.addstr(
