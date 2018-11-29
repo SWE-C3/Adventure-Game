895691f90838922da59e84d7f375168be0bfb04e
MichaelLubos <lubii@gmx.de>
2018-11-29 11:22:14 +0100

    uses curses to print inventory now

--- src/inventory.py
+++ src/inventory.py
@@ -18,7 +18,25 @@ class Inventory(object):
         self.screen.clear()
         size = self.screen.getmaxyx()
         inventory_screen = curses.newwin(size[0], size[1], 0, 0)
-        draw_table_centered(inventory_screen, ['Name', 'Heilungsmenge'], self.items.name)
+
+        # print Cookies
+        inventory_screen.addstr(0, 4, 'Cookies')
+        inventory_screen.addstr(1, 1, '\t'.join(['Name', 'Heilungswert']))
+        i = 2
+        for item in self.items.values():
+            inventory_screen.addstr(i, 2, '\t'.join([str(x) for x in [item.name, item.heal_value]]))
+            i = i + 1
+
+        # print Equipment
+        inventory_screen.addstr(0, 44, 'Equipment')
+        inventory_screen.addstr(1, 41, '\t'.join(['Name', 'Stärke']))
+        i = 2
+        for equipment in self.equipment.values():
+            inventory_screen.addstr(i, 42, '\t'.join([str(x) for x in [equipment.name, equipment.strength]]))
+            i = i + 1
+
+        inventory_screen.refresh()
+        inventory_screen.getch()
 
     # adds Item(Healing) to Inventory
     def add_item(self, item):
@@ -27,4 +45,3 @@ class Inventory(object):
     # adds Equipment to Inventory
     def add_equipment(self, equipment):
         self.equipment[equipment.name] = equipment
-
