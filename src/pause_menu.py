8779163546d1951715abe7f0d4cff6c637d91f86
PascalTruglio <pascaltruglio@freenet.de>
2018-11-29 07:32:14 +0100

    v3

--- src/pause_menu.py
+++ src/pause_menu.py
@@ -1,6 +1,15 @@
+"""
+Interfaces for the pause menu
+"""
+
 import curses
 
-class PascalCase:
+
+class PauseMenu:
+    """
+    Interface class for pause menu
+    """
+
     def __init__(self, screen):
         self.menu_items = ["[Z] Zurück zur Karte",
                            "[S] Speicherstand laden", "[Q] Spiel verlassen"]
@@ -8,15 +17,23 @@ class PascalCase:
         self.pressed_key = ord('z')
 
     def print(self):
+        """
+        render pause menu to terminal window
+        """
+
+        # clear current screen
         self.screen.clear()
+        # get a Tupel (y, x) - height, width of the window
         size = self.screen.getmaxyx()
 
+        # create new window for menu
         menu_item_win = curses.newwin(size[0], size[1], 0, 0)
-        yPosOffset = (size[0] // 2) - 2
+        # y_pos_offset to set items vertical below each other
+        ypos = (size[0] // 2) - 2
 
         for item in self.menu_items:
-            menu_item_win.addstr(yPosOffset, int(
-                size[1] / 2) - (len(item) // 2), item)
-            yPosOffset += 1
+            menu_item_win.addstr(ypos, (
+                size[1] // 2) - (len(item) // 2), item)
+            ypos += 1
 
         menu_item_win.refresh()
