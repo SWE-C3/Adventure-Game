8178715129a7ff86c93832ba5678f5e12e2eee13
Knut Zuidema <knut.zuidema@gmail.com>
2018-11-30 20:55:18 +0100

    add docstring to option_dialog()

--- src/utility.py
+++ src/utility.py
@@ -53,6 +53,16 @@ def draw_table_centered(window, headers: List[str], items: List[Tuple[str]]):
 
 def option_dialog(window, question: str, options: List[str],
                   dialog_width: int = 40):
+    """
+    create a dialog with multiple options centered in the specified window
+    :param window: window to be centered on
+    :param question: question to be asked
+    :param options: options to be provided, options are required to be shorter
+                    than dialog_width
+    :param dialog_width: width of the dialog window, question will be wrapped
+                         to fit the width
+    :return: a window object containing the dialog
+    """
     assert max(len(option) for option in options) <= dialog_width
     height, width = window.getmaxyx()
     question_lines = textwrap.wrap(question, dialog_width - 2)
