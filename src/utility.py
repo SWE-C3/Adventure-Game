dbcf4f75ad53a44cdd0080ed42d2b0b56e42ef4d
Knut Zuidema <knut.zuidema@gmail.com>
2018-11-30 20:49:43 +0100

    reduce number of local variables

--- src/utility.py
+++ src/utility.py
@@ -22,33 +22,32 @@ def draw_table_centered(window, headers: List[str], items: List[Tuple[str]]):
     height, width = window.getmaxyx()
     table_height = len(items) + 5
     table_width = sum(max_column_lengths) + 3 * len(headers) + 1
-    y_position = height // 2 - table_height // 2
-    x_position = width // 2 - table_width // 2
-    table = curses.newwin(table_height, table_width, y_position, x_position)
-
-    separator_top = (
-        f'┌─{"─┬─".join("─" * length for length in max_column_lengths)}─┐'
-    )
-    separator_middle = (
-        f'├─{"─┼─".join("─" * length for length in max_column_lengths)}─┤'
-    )
-    separator_bottom = (
-        f'└─{"─┴─".join("─" * length for length in max_column_lengths)}─┘'
-    )
+    table = curses.newwin(table_height, table_width,
+                          height // 2 - table_height // 2,
+                          width // 2 - table_width // 2)
 
     def template(values):
         columns = [f'{value:^{max_length}}'
                    for value, max_length in zip(values, max_column_lengths)]
         return f'│ {" │ ".join(columns)} │'
 
-    table.addstr(0, 0, separator_top)
+    table.addstr(
+        0, 0,
+        f'┌─{"─┬─".join("─" * length for length in max_column_lengths)}─┐'
+    )
     table.addstr(1, 0, template(headers))
-    table.addstr(2, 0, separator_middle)
+    table.addstr(
+        2, 0,
+        f'├─{"─┼─".join("─" * length for length in max_column_lengths)}─┤'
+    )
     y_position = 3
     for item in items:
         table.addstr(y_position, 0, template(item))
         y_position += 1
-    table.addstr(y_position, 0, separator_bottom)
+    table.addstr(
+        y_position, 0,
+        f'└─{"─┴─".join("─" * length for length in max_column_lengths)}─┘'
+    )
     return table
 
 
