ecb630448aceecd0079056e03d751fcc08d1c0cc
PascalTruglio <pascaltruglio@freenet.de>
2018-12-03 21:09:18 +0100

    add set.strength

--- src/player.py
+++ src/player.py
@@ -19,15 +19,24 @@ class Player:
             position = [0, 0]
         self.position = position
         self.health = 10
-        self.strength = 0  # Default Stärke wv?
-        self.item = ""  # Default Item wäre?
+        self.strength = 0  # Default strength
+        self.item = "" 
+
 
     def set_pos(self, x, y):
         """
         set_pos sets the current position of Player.
-        :param x
-        :param y:
+        :param x: horizontal position of Player
+        :param y: vertical position of Player
         :return:
         """
         self.position[0] = x
         self.position[1] = y
+
+
+    def set_strength(self,x):
+        """
+        set_strength set the strength of Player
+        :param: x added the strength of an item to Player
+        """
+        self.strength = self.strength + x
