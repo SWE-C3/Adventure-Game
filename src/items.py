a6e4506c4d45c24c76a1494150ac8f520d6a62ad
Tim Goroncy <tim.goroncy@alumni.fh-aachen.de>
2018-12-06 10:20:33 +0100

    no normal setter, corrected  auto setter

--- src/items.py
+++ src/items.py
@@ -3,22 +3,16 @@ import curses
 import json
 import random
 
+
 class Item:
     """
     Item main class
     """
-    
+
     def __init__(self):
         self.name
 
-    def set(self,name):
-        self.name = name
-    
-    # automatically sets the values according to the level
-    def set_auto(self,level_number):
-        set_random_name()
-        self.health = random.randint(((level_number-1)*10),(level_number*10)) # temporary calculation
-        
+
 class Healing_Item(Item):
     """
     Healing_Item is a subclass of Item with the additional attribute health
@@ -28,15 +22,18 @@ class Healing_Item(Item):
         super().__init__
         self.health = 0
 
-    def set(self,name,health):
-        super.set(name)
-        self.health = health
-    
     # randomly sets a name from the given json file
     def set_random_name(self):
         with open("items.json", "r") as read_file:
             data = json.load(read_file)
         self.name = random.choice(data["Healing_name"])
+
+    # automatically sets the values according to the level
+    def set_auto(self, level_number):
+        set_random_name()
+        self.health = random.randint(
+            ((level_number-1)*10), (level_number*10))  # temporary calculation
+
 
 class Equipment(Item):
     """
@@ -47,14 +44,17 @@ class Equipment(Item):
         super().__init__
         self.strength = 0
 
-    def set(name,strength):
-        super.set(name)
-        self.strength = strength
-
+    # randomly sets a name from the given json file
     def set_random_name(self):
         with open("items.json", "r") as read_file:
             data = json.load(read_file)
         self.name = random.choice(data["Equipment_name"])
+
+    # automatically sets the values according to the level
+    def set_auto(self, level_number):
+        set_random_name()
+        self.health = random.randint(
+            ((level_number-1)*10), (level_number*10))  # temporary calculation
 
 
 class Weapon(Item):
@@ -67,20 +67,14 @@ class Weapon(Item):
         super().__init__
         self.strength = 0
 
-    def set(self,name,strength):
-        super.set(name)
-        self.strength = strength
-
+    # randomly sets a name from the given json file
     def set_random_name(self):
         with open("items.json", "r") as read_file:
             data = json.load(read_file)
         self.name = random.choice(data["Weapon_name"])
 
-
-
-
-        
-
-
-
-
+    # automatically sets the values according to the level
+    def set_auto(self, level_number):
+        set_random_name()
+        self.health = random.randint(
+            ((level_number-1)*10), (level_number*10))  # temporary calculation
