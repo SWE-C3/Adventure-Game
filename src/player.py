9ba256ed092c5a76bf14b8930243ffa4fafec31e
MuusKaan <muus.kaan@hotmail.com>
2018-12-06 14:09:31 +0100

    Modified player

--- src/player.py
+++ src/player.py
@@ -7,7 +7,7 @@ class Player:
     """
     This class defines the features and characteristics of an object Player.
     """
-    DEFAULT_POSITION = [0, 0]  # Default position of Player [x,y]
+    DEFAULT_POSITION = [0, 0, 0]  # Default position of Player [x,y,Level]
     DEFAULT_HEALTH = 10  # Default health of Player
     DEFAULT_STRENGTH = 5  # Default strength of Player
 
@@ -15,9 +15,9 @@ class Player:
         if position is None:
             position = self.DEFAULT_POSITION
         self.position = position
-        # Item Array of Player,0: Head,1: Chest, 2: Trousers,
-        # 3: Shoes, 4: Weapon, 5-7: Cookies
-        self.items = ["", "", "", "", "", "", "", ""]
+        self.health = self.DEFAULT_HEALTH
+        self.items = []  # 0: Head, 1: Chest, 2: Trousers, 3: Shoes, 4: Weapon
+        self.cookies = []
 
     @property
     def strength(self):
@@ -27,24 +27,4 @@ class Player:
         # The actual strength calculated by list slice:
         # DEFAULT_STRENGTH + sum of the items 0 till 4(5-1)
         # Head + Chest + Trousers + Shoes + Weapon items.
-        return self.DEFAULT_STRENGTH + sum(self.items[0:5])
-
-    @property
-    def health(self):
-        """
-        This defines the health of Player
-        """
-        # The actual health calculated by list slice:
-        # DEFAULT_HEALTH + sum of the items 5 till 7(8-1)
-        # items.Cookies
-        return self.DEFAULT_HEALTH + sum(self.items[5:8])
-
-    def update_pos(self, x_axis, y_axis):
-        """
-        set_pos helps setting the current coordinates of Player position.
-        :param x_axis: horizontal position of Player
-        :param y_axis: vertical position of Player
-        :return:
-        """
-        self.position[0] = x_axis
-        self.position[1] = y_axis
+        return self.DEFAULT_STRENGTH + sum(self.items)
