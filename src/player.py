c2ed5f55519b36d38a5d468a9b1592f8efb8b20d
MuusKaan <muus.kaan@hotmail.com>
2018-12-06 14:44:20 +0100

    Changed default variable names into lower case

--- src/player.py
+++ src/player.py
@@ -7,15 +7,15 @@ class Player:
     """
     This class defines the features and characteristics of an object Player.
     """
-    DEFAULT_POSITION = [0, 0, 0]  # Default position of Player [x,y,Level]
-    DEFAULT_HEALTH = 10  # Default health of Player
-    DEFAULT_STRENGTH = 5  # Default strength of Player
+    default_position = [0, 0, 0]  # Default position of Player [x,y,Level]
+    default_health = 10  # Default health of Player
+    default_strength = 5  # Default strength of Player
 
     def __init__(self, position=None):
         if position is None:
-            position = self.DEFAULT_POSITION
+            position = self.default_position
         self.position = position
-        self.health = self.DEFAULT_HEALTH
+        self.health = self.default_health
         self.items = []  # 0: Head, 1: Chest, 2: Trousers, 3: Shoes, 4: Weapon
         self.cookies = []
 
@@ -24,7 +24,6 @@ class Player:
         """
         This defines the strength of Player
         """
-        # The actual strength calculated by list slice:
-        # DEFAULT_STRENGTH + sum of the items 0 till 4(5-1)
-        # Head + Chest + Trousers + Shoes + Weapon items.
-        return self.DEFAULT_STRENGTH + sum(self.items)
+        # default_strength + sum of the items:
+        # Head + Chest + Trousers + Shoes + Weapon
+        return self.default_strength + sum(self.items)
