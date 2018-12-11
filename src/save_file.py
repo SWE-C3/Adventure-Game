130741ff255e6bd866302edd37941d8f67feda95
Tobias Ansey <tobias.ansey@outlook.com>
2018-12-11 22:52:05 +0100

    change SaveFile class (-> JSON format)

--- src/save_file.py
+++ src/save_file.py
@@ -1,59 +1,30 @@
 """
-Methods for handling a save file of the player's current progress
+Contains SaveFile class
 """
 
-from src import player
+import json
+from src.player import Player
 
 
 class SaveFile:
     """
     Defines methods for transferring information between
-    player class attributes and a textfile
+    player class attributes and a JSON file
     """
 
-    @staticmethod
-    def write_file():
+    def __init__(self):
+        self.playerinstance = Player()
+
+    def write_file(self):
         """
-        Transfers relevant player attribute values into a textfile
+        Write player class information into a JSON file
         """
-        with open("save_file.txt", "w") as file:
-            file.write("Ebene:\n" + player.Player.position[2] + "\n\n")
+        with open("save_file.json", "w") as file:
+            file.write(json.dumps(vars(self.playerinstance), indent=4))
 
-            file.write("Position:\n" + player.Player.position[0] + "\n" +
-                       player.Player.position[1] + "\n\n")
-
-            file.write("Lebenspunkte:\n" + str(player.Player.health) + "\n\n")
-
-            file.write("Items:\n")
-            for i in player.Player.items:
-                file.write(i + "\n")
-            file.write("\n")
-
-            file.write("Cookies:\n")
-            for i in player.Player.cookies:
-                file.write(i + "\n")
-
-    @staticmethod
-    def read_file():
+    def read_file(self):
         """
-        Transfers relevant lines from a given textfile into
-        player attribute values
+        Read player class information from a JSON file
         """
-        with open("save_file.txt", "r") as file:
-            stats = file.readlines()
-
-            for i in range(0, 21):
-                stats[i] = stats[i].rstrip('\n')
-
-            player.Player.position[2] = stats[1]
-
-            player.Player.position[0] = stats[4]
-            player.Player.position[1] = stats[5]
-
-            player.Player.health = stats[8]
-
-            for i in range(0, 5):
-                player.Player.items[i] = stats[i+11]
-
-            for i in range(0, 3):
-                player.Player.cookies[i] = stats[i+18]
+        with open("save_file.json") as file:
+            self.playerinstance.__dict__ = json.load(file)
