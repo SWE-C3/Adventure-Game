d018a6e23f727124cd3f582216f5ef7bf7d5af49
Tim Goroncy <tim.goroncy@alumni.fh-aachen.de>
2018-12-16 15:00:22 +0100

    new solution

--- src/items.py
+++ src/items.py
@@ -1,8 +1,8 @@
-"""
-Item class
-"""
 import json
+import test
 import random
+
+from enum import Enum
 
 
 class Item:
@@ -13,28 +13,31 @@ class Item:
     def __init__(self):
         self.name = None
 
+    def set(self, name):
+        self.name = name
 
-class HealingItem(Item):
+
+class Healing_Item(Item):
     """
     Healing_Item is a subclass of Item with the additional attribute health
     """
 
     def __init__(self):
-        super().__init__()
+        self.name = None
         self.health = None
 
+    def set(self, name, health):
+        super.set(name)
+        self.health = health
+
+    # randomly sets a name from the given json file
     def set_random_name(self):
-        """
-        randomly sets a name from the given json file
-        """
-        with open("items.json", "r") as read_file:
+        with open("../resources/items.json", "r") as read_file:
             data = json.load(read_file)
         self.name = random.choice(data["Healing_name"])
 
+    # automatically sets the values according to the level
     def set_auto(self, level_number):
-        """
-        automatically sets the values according to the level
-        """
         self.set_random_name()
         self.health = random.randint(
             ((level_number-1)*10), (level_number*10))  # temporary calculation
@@ -42,52 +45,23 @@ class HealingItem(Item):
 
 class Equipment(Item):
     """
-    Equipment is a subclass of Item with the additional attribute strength
+    Equipment is a subclass of Item with the additional attribute strength, 
+    you have to set a predefined part for initialization
     """
 
     def __init__(self):
-        super().__init__()
-        self.strength = None
+        super().__init__
+        self.strength = 0
+        self.part = Enum("Part", "Weapon Helmet Body Pants Shoes")
 
     def set_random_name(self):
-        """
-        randomly sets a name from the given json file
-        """
-        with open("items.json", "r") as read_file:
+        with open("../resources/items.json", "r") as read_file:
             data = json.load(read_file)
-        self.name = random.choice(data["Equipment_name"])
+        file_name = self.part + "_name"
+        self.name = random.choice(data[file_name])
 
+    # automatically sets the values according to the level
     def set_auto(self, level_number):
-        """
-        automatically sets the values according to the level
-        """
         self.set_random_name()
-        self.strength = random.randint(
-            ((level_number-1)*10), (level_number*10))  # temporary calculation
-
-
-class Weapon(Item):
-    """
-    Weapon is a subclass of Item with the additional attribute strength,
-    and is meant to separate weapons and equipment for the inventory
-    """
-
-    def __init__(self):
-        super().__init__()
-        self.strength = None
-
-    def set_random_name(self):
-        """
-        randomly sets a name from the given json file
-        """
-        with open("items.json", "r") as read_file:
-            data = json.load(read_file)
-        self.name = random.choice(data["Weapon_name"])
-
-    def set_auto(self, level_number):
-        """
-        automatically sets the values according to the level
-        """
-        self.set_random_name()
-        self.strength = random.randint(
+        self.health = random.randint(
             ((level_number-1)*10), (level_number*10))  # temporary calculation
