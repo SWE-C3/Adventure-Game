54eb0450278dadfc1522c379bb00d4f118f362df
Oschmann <o.oschmann@gmx.de>
2018-12-27 10:03:23 +0100

    ladder-events added

---
+++ src/ladder_up_event.py
@@ -0,0 +1,25 @@
+"""
+Event for moving one level up via ladder
+"""
+
+
+class LadderUpEvent:
+    """
+    This class defines the ladder down event
+    """
+
+    def __init__(self, screen):
+        self.screen = screen
+        self.top = "--- Leiter-hoch-Event ---"
+        self.header = [
+            "Du hast eine Leiter nach oben gefunden."
+        ]
+
+    @classmethod
+    def change_level(cls, player):
+        """
+        changes the current Level
+        """
+        current_position = player.get_postion()
+        current_position[2] += 1
+        player.set_postition(current_position)
